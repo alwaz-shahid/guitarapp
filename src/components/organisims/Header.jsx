310b6875072ecd1e96399ba4f37460c3d3fab02f
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2023-04-16 18:19:37 +0500

    ui

--- src/components/organisims/Header.jsx
+++ src/components/organisims/Header.jsx
@@ -4,7 +4,10 @@ export default function Header() {
   return (
     <header className='sticky top-0 z-50  p-1 shadow-2xl'>
       <nav className='px-4 flex justify-between'>
-        <h1 className='font-extrabold'>Guitar App</h1> <p>nav</p>
+        <h1 className='font-extrabold italic animate-pulse'>Gitarr </h1>{' '}
+        <p className='px-2 text-xs md:text-sm font-bold py-1 rounded-md bg-yellow-200'>
+          BETA
+        </p>
       </nav>
     </header>
   );
--- src/components/screens/WelcomeScreen.jsx
+++ src/components/screens/WelcomeScreen.jsx
@@ -12,15 +12,23 @@ export default function WelcomeScreen() {
   };
 
   return (
-    <section className='min-h-[80vh] p-5 w-full flex justify-between'>
-      <div className='min-w-[60%] flex items-start justify-center flex-col space-y-5'>
+    <section className='min-h-[80vh] p-5 w-full flex flex-col md:flex-row justify-between bg'>
+      <div className='min-w-[60%] flex items-start justify-center flex-col space-y-5 p-5'>
         <h1 className='text-7xl'>Welcome to Gitarr</h1>
-        <h2 className='text-3xl p-1'>
+        <h2 className='text-3xl p-1  md:max-w-lg '>
           Empowering Your Guitar Journey with Innovative Tools.
         </h2>
+        <p className=' p-1  md:max-w-lg'>
+          We're excited to help you on your guitar journey. With our upcoming
+          tools, we're confident that we can take your playing to the next
+          level.{' '}
+        </p>
+        <button className='px-4 py-2 mx-1 rounded-md bg-yellow-300 '>
+          Call to action
+        </button>
       </div>
-      <div className='p-2 '>
-        <Lottie options={defaultOptions} height={400} width={400} />
+      <div className='md:p-2 '>
+        <Lottie options={defaultOptions} height={500} width={400} />
       </div>
       <div className=''></div>
     </section>
